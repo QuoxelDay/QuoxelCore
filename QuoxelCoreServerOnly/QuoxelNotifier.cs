ec54259f90c75a4496d9da38741555fa762fd073
wwwDayDream <13383838+wwwDayDream@users.noreply.github.com>
2025-02-09 03:48:58 -0600

    Updated to utilize Discord.Net & updated 'common'

--- QuoxelCoreServerOnly/QuoxelNotifier.cs
+++ QuoxelCoreServerOnly/QuoxelNotifier.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using CSharpDiscordWebhook.NET.Discord;
 using QuoxelCore;
 
 namespace QuoxelCoreServer;
@@ -27,59 +26,44 @@ public static class QuoxelNotifier
         }
     }
 
-    private static DiscordWebhook? _webhook = null;
-    public static DiscordWebhook? Webhook
-    {
-        get
-        {
-            if (_webhook == null && string.IsNullOrEmpty(Config.DiscordWebhookUri)) return null;
-            return _webhook ??= new DiscordWebhook { Uri = new Uri(Config.DiscordWebhookUri) };
-        }
-    }
-
-    private enum NotificationType { Simple, Info, Warning, Error, Critical }
+    
+    
+    
+    
+    
+    
+    private enum NotificationType { Simple, Info, Warn, Error, Crash }
 
     private static void Notify(NotificationType type, string content)
     {
         var emoji = type switch
         {
             NotificationType.Info => ":information_source:",
-            NotificationType.Warning => ":warning:",
+            NotificationType.Warn => ":warning:",
             NotificationType.Error => ":interrobang:",
-            NotificationType.Critical =>  ":name_badge:",
+            NotificationType.Crash =>  ":name_badge:",
             NotificationType.Simple => "",
             _ => ""
         };
         var mentions = type switch
         {
             NotificationType.Info => "",
-            NotificationType.Warning => "<@&1336856485191356416>\t",
+            NotificationType.Warn => "<@&1336856485191356416>\t",
             NotificationType.Error => "<@&1336858530321404014>, <@&1336856485191356416>\t",
-            NotificationType.Critical =>  "<@&1336858530321404014>, <@&1336856485191356416>\t",
+            NotificationType.Crash =>  "<@&1336858530321404014>, <@&1336856485191356416>\t",
             NotificationType.Simple => "",
             _ => ""
         };
-        
-        var message = new DiscordMessage
-        {
-            Content = mentions
-        };
-        if (type == NotificationType.Simple)
-        {
-            message.Content = content;
-        }
-        else
-        {
-            message.Embeds.Add(
-                new DiscordEmbed
-                {
-                    Timestamp = new DiscordTimestamp(DateTime.Now),
-                    Title = $"{emoji} {type}",
-                    Description = content
-                });
-        }
-
-        Webhook?.SendAsync(message);
+        //
+        // var message = new DiscordMessage
+        // {
+        //     Content = mentions
+        // };
+        // message.Content = 
+        //     type == NotificationType.Simple ? 
+        //         content : $"{emoji} **{type}**\n`{content.Trim('\n')}` {mentions}";
+        //
+        // Webhook?.SendAsync(message);
     }
 
     private static void NotifyProcessor(NotificationType type, string? errorMessage = null, Exception? exception = null,
@@ -97,9 +81,9 @@ public static class QuoxelNotifier
     public static void NotifyInfo(string? message = null, string? typeName = null, string? methodName = null) =>
         NotifyProcessor(NotificationType.Info, message, null, typeName, methodName);
     public static void NotifyWarning(string? errorMessage = null, Exception? exception = null, string? typeName = null, string? methodName = null) =>
-        NotifyProcessor(NotificationType.Warning, errorMessage, exception, typeName, methodName);
+        NotifyProcessor(NotificationType.Warn, errorMessage, exception, typeName, methodName);
     public static void NotifyError(string? errorMessage = null, Exception? exception = null, string? typeName = null, string? methodName = null) =>
         NotifyProcessor(NotificationType.Error, errorMessage, exception, typeName, methodName);
     public static void NotifyCritical(string? errorMessage = null, Exception? exception = null, string? typeName = null, string? methodName = null) =>
-        NotifyProcessor(NotificationType.Critical, errorMessage, exception, typeName, methodName);
+        NotifyProcessor(NotificationType.Crash, errorMessage, exception, typeName, methodName);
 }
