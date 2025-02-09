006e97f245dfea09eea434c5bc10571bdb72b608
wwwDayDream <13383838+wwwDayDream@users.noreply.github.com>
2025-02-08 22:02:32 -0600

    Regular progress update (I know, bad commit message)

---
+++ QuoxelCoreShared/QuoxelCoreServer.cs
@@ -0,0 +1,12 @@
+﻿using Vintagestory.API.Common;
+
+namespace QuoxelCore;
+
+public class QuoxelCoreServer : ModSystem
+{
+    public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Server;
+    public override void StartPre(ICoreAPI api)
+    {
+        api.Logger.Debug("LOADED MY CUSTOM QUOXEL_CORE_SERVER");
+    }
+}
